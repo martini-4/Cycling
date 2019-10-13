a14f634b76851c2c8ddcbf3bbec9fb7d1b9d711e
吉松拓巳 <matty@mattymacbookpro.local>
2019-10-13 13:04:58 +0900

    [add]オーナー編集画面作成

--- app/controllers/owner/owners_controller.rb
+++ app/controllers/owner/owners_controller.rb
@@ -5,14 +5,27 @@ class Owner::OwnersController < ApplicationController
   end
 
   def edit
-  end
-
-  def withdrawal
+    @owner = Owner.find(params[:id])
   end
 
   def update
+    @owner = Owner.find(params[:id])
+    if @owner.update(owner_params)
+      flash[:success] = 'オーナー情報を編集しました。'
+       redirect_to owner_owner_path(@owner.id)
+     else
+       flash[:danger] = "編集に失敗しました。"
+       redirect_to edit_owner_owner_path
+    end
   end
 
   def destory
   end
+
+  def withdrawal
+  end
+  protected
+    def owner_params
+      params.require(:owner).permit(:nickname, :sex_id)
+    end
 end

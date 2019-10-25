0a4cb684626d76e66f6b6297becd161d438ca4de
吉松拓巳 <matty@mattymacbookpro.local>
2019-10-25 12:25:23 +0900

    [add]adminユーザー(一覧・マイページ作成)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,24 @@
 class Admin::UsersController < ApplicationController
   def index
+  	@user = User.all.page(params[:page]).per(30)
   end
 
   def show
+    @user = User.find(params[:id])
+    @contracts = @user.contracts
+
+    # カレンダーイベント表示
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render :show, status: :ok, location: @contracts.to_json }
+    end
+  end
+
+  def destroy
+  	user = User.find(params[:id])
+    user.destroy
+    flash[:success] = 'ユーザーを削除しました。'
+    redirect_to admin_users_path
   end
 end
+
